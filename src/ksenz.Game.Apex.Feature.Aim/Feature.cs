1407156186175da484d81d18c7977b3ecd3cec04
krobicon <113786742+krobicon@users.noreply.github.com>
2023-02-20 18:08:17 -0500

    Update Feature.cs

--- src/ksenz.Game.Apex.Feature.Aim/Feature.cs
+++ src/ksenz.Game.Apex.Feature.Aim/Feature.cs
@@ -148,7 +148,7 @@ namespace ksenz.Game.Apex.Feature.Aim
                     _targetPreviousTicks = frameTime.Ticks;
                     _targetPreviousVecPunchWeaponAngle = localPlayer.VecPunchWeaponAngle;
                 }
-                else if (MathF.Abs(localPlayer.ViewAngle.X - AdjustSelf(localPlayer).GetDesiredAngle(AdjustTarget(_target)).X) >= (_config.PitchAngle * 10/(localPlayer.LocalOrigin.Distance2(target.LocalOrigin) * Constants.UnitToMeter)) || MathF.Abs(localPlayer.ViewAngle.Y - AdjustSelf(localPlayer).GetDesiredAngle(AdjustTarget(_target)).Y) >= (_config.YawAngle * 10/(localPlayer.LocalOrigin.Distance2(target.LocalOrigin) * Constants.UnitToMeter)))
+                else if (MathF.Abs(localPlayer.ViewAngle.X - AdjustSelf(localPlayer).GetDesiredAngle(AdjustTarget(_target)).X) >= (_config.PitchAngle * 10/(localPlayer.LocalOrigin.Distance2(_target.LocalOrigin) * Constants.UnitToMeter)) || MathF.Abs(localPlayer.ViewAngle.Y - AdjustSelf(localPlayer).GetDesiredAngle(AdjustTarget(_target)).Y) >= (_config.YawAngle * 10/(localPlayer.LocalOrigin.Distance2(_target.LocalOrigin) * Constants.UnitToMeter)))
                 {
                     var target = Find(state, localPlayer, targetType == TargetType.All);
                     if (target == null) return;
