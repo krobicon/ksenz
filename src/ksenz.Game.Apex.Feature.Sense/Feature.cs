1af79b15a60506a88299b91f081c538f47235823
krobicon <113786742+krobicon@users.noreply.github.com>
2023-02-21 18:31:47 -0500

    Update Feature.cs

--- src/ksenz.Game.Apex.Feature.Sense/Feature.cs
+++ src/ksenz.Game.Apex.Feature.Sense/Feature.cs
@@ -86,7 +86,7 @@ namespace ksenz.Game.Apex.Feature.Sense
                 }
                 if (force)
                 {
-                    int st;
+                    uint st;
                     if (press && !release) 
                     {
 			            st = 5;
