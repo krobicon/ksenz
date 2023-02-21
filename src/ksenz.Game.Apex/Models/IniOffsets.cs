00f5afeba69ad0bdb89f8919dbd696c046b65ee8
krobicon <113786742+krobicon@users.noreply.github.com>
2023-02-20 22:37:31 -0500

    Update IniOffsets.cs

--- src/ksenz.Game.Apex/Models/IniOffsets.cs
+++ src/ksenz.Game.Apex/Models/IniOffsets.cs
@@ -39,7 +39,9 @@ namespace ksenz.Game.Apex.Models
         public uint ButtonInSpeed => _ini.Get("Buttons", "in_speed");
         public uint ButtonInWalk => _ini.Get("Buttons", "in_walk");
         public uint ButtonInZoom => _ini.Get("Buttons", "in_zoom");
-        public uint ButtonInForward => _ini.Get("Buttons", "in_forward");
+        public uint ButtonInForwardDown1 => _ini.Get("Buttons", "in_forward");
+        public uint ButtonInForwardDown2 => _ini.Get("Buttons", "in_forward") + 0x4;
+        public uint ButtonInForwardState => _ini.Get("Buttons", "in_forward") + 0x8;
         public uint CoreEntityList => _ini.Get("Miscellaneous", "cl_entitylist");
         public uint CoreLevelName => _ini.Get("Miscellaneous", "LevelName");
         public uint CoreLocalPlayer => _ini.Get("Miscellaneous", "LocalPlayer") + 0x8;
