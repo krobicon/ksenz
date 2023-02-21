21ccb69cef0a30eccab15bd31ed3ef2b0eb550f1
krobicon <113786742+krobicon@users.noreply.github.com>
2023-02-21 17:08:21 -0500

    Update Feature.cs

--- src/ksenz.Game.Apex.Feature.Sense/Feature.cs
+++ src/ksenz.Game.Apex.Feature.Sense/Feature.cs
@@ -27,7 +27,7 @@ namespace ksenz.Game.Apex.Feature.Sense
                 {
                     if (player.IsValid() && !player.IsSameTeam(localPlayer))
                     {
-                        if (localPlayer.LocalOrigin.Distance2(player.LocalOrigin) * Constants.UnitToMeter < _config.Distance && (state.Buttons.InForwardState !=0 && state.Buttons.InForwardState < 10))
+                        if (localPlayer.LocalOrigin.Distance2(player.LocalOrigin) * Constants.UnitToMeter < _config.Distance && (state.Buttons.InForwardDown1 ==0 && state.Buttons.InForwardDown2 == 0))
                         {
                             player.GlowEnable = (byte)(player.Visible ? 5 : 7);
                             if (_config.Distance != 333)
