52f719a4c77f87711bf3c3b2d04a4d00ce1aff36
krobicon <113786742+krobicon@users.noreply.github.com>
2023-02-22 20:09:48 -0500

    Update Feature.cs

--- src/ksenz.Game.Apex.Feature.Sense/Feature.cs
+++ src/ksenz.Game.Apex.Feature.Sense/Feature.cs
@@ -48,7 +48,7 @@ namespace ksenz.Game.Apex.Feature.Sense
                         }
                     }
                 }
-                if (!localPlayer->IsGrounded())
+                if (!localPlayer.IsGrounded())
                 {
                     if (state.Buttons.InForwardState == 0)
                     {
