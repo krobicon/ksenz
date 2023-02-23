56e1d656b2456cff54f481e1c911ac9ef558defd
krobicon <113786742+krobicon@users.noreply.github.com>
2023-02-22 21:41:22 -0500

    Update Player.cs

--- src/ksenz.Game.Apex.Core/Models/Player.cs
+++ src/ksenz.Game.Apex.Core/Models/Player.cs
@@ -23,6 +23,7 @@ namespace ksenz.Game.Apex.Core.Models
         private readonly Access<Vector> _vecPunchWeaponAngle;
         private readonly Access<Vector> _viewAngle;
         private readonly Access<uint> _flags;
+        private readonly Access<Vector> _glowColor;
 
         #region Constructors
 
@@ -40,6 +41,7 @@ namespace ksenz.Game.Apex.Core.Models
             _vecPunchWeaponAngle = driver.Access(address + offsets.PlayerVecPunchWeaponAngle, VectorType.Instance);
             _viewAngle = driver.Access(address + offsets.PlayerViewAngle, VectorType.Instance);
             _flags = driver.Access(address + offsets.PlayerFlags, UInt32Type.Instance);
+            _glowColor = driver.Access(address + offsets.PlayerGlowColor, VectorType.Instance);
         }
 
         #endregion
@@ -141,6 +143,13 @@ namespace ksenz.Game.Apex.Core.Models
             get => _flags.Get();
             set => _flags.Set(value);
         }
+        
+        [JsonPropertyName("glowColor")]
+	    public Vector GlowColor
+	    {
+	        get => _glowColor.Get();
+	        set => _glowColor.Set(value);
+	    }
 
         [JsonPropertyName("visible")]
         public bool Visible => _lastVisibleTime.Visible;
@@ -163,6 +172,7 @@ namespace ksenz.Game.Apex.Core.Models
             _vecPunchWeaponAngle.Update(frameTime);
             _viewAngle.Update(frameTime);
             _flags.Update(frameTime);
+            _glowColor.Update(frameTime);
         }
 
         #endregion
